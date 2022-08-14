82f29f1d9b958f3f9e8811a508b38afcdbeb53ab
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-08-14 19:27:55 +0200

    fixs

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,7 +2,6 @@ import * as basicLightbox from 'basiclightbox';
 export const instance = basicLightbox.create(
   `
     <div class="modal">
-
       <div class="question">
           <p>Хочете змінити таймер?</p>
           <button type="button" data-yes>Так</button>
