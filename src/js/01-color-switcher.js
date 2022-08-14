61e74268c4df2691017129cc670ef2fd7792951a
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-08-14 18:17:10 +0200

    add 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let intervalId = null;
+
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', handlerOnClickStart);
+btnStop.addEventListener('click', handlerOnClickStop);
+
+function handlerOnClickStart() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  changeColorBody();
+}
+function handlerOnClickStop() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  
+  clearInterval(intervalId);
+}
+
+function changeColorBody() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
